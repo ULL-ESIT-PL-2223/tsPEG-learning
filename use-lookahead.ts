ea876e86b180d8e929a65267efc44649c263ec81
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-09-03 13:44:27 +0100

    lookahead.peg

---
+++ use-lookahead.ts
@@ -0,0 +1,12 @@
+import { inspect } from 'util'
+import { parse } from './lookahead-parser'
+const inputs = ['aabbcc', 'aabbc']
+
+for (let input of inputs) {
+  let r = parse(input)
+  if (r.ast) {
+    console.log(`INPUT: ${input}\nast:\n${inspect(r.ast, { depth: null })}`)
+  } else {
+    console.error(`INPUT: ${input}\nerror:\n${inspect(r.errs, { depth: null })}`)
+  }  
+}
