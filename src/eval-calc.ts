74c15a2c20886a0d08b75029f005cbe030463e83
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-09-03 21:35:22 +0100

    adding src/calc-grammar.peg example

---
+++ src/eval-calc.ts
@@ -0,0 +1,36 @@
+import * as Parser from './calc-grammar';
+
+export function evaluate(input : string) : number | null {
+    const p = new Parser.Parser(input);
+    const tree = p.parse();
+    if(tree.errs.length === 0 && tree.ast)
+        return calcSum(tree.ast);
+    console.log('' + tree.errs);
+    return null;
+}
+
+function calcInt(at : Parser.INT) : number {
+    return parseInt(at.val);
+}
+
+function calcAtom(at : Parser.ATOM) : number {
+    if(at.kind === Parser.ASTKinds.ATOM_1)
+        return calcInt(at.val);
+    return calcSum(at.val);
+}
+
+function calcFac(at : Parser.FAC) : number {
+    return at.tail.reduce((x, y) => {
+        if(y.op === '*')
+            return x * calcAtom(y.sm);
+        return x / calcAtom(y.sm);
+    }, calcAtom(at.head));
+}
+
+function calcSum(at : Parser.SUM) : number {
+    return at.tail.reduce((x, y) => {
+        if(y.op === '+')
+            return x + calcFac(y.sm);
+        return x - calcFac(y.sm);
+    }, calcFac(at.head));
+}
